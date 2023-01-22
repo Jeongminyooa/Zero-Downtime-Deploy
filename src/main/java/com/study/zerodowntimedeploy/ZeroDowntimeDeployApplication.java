d278366dad3370d641ad7638607ff971453f246b
Jeongminyooa <jmjmjmz732002@naver.com>
2023-01-23 01:53:51 +0900

    config Datasource

--- src/main/java/com/study/zerodowntimedeploy/ZeroDowntimeDeployApplication.java
+++ src/main/java/com/study/zerodowntimedeploy/ZeroDowntimeDeployApplication.java
@@ -2,8 +2,9 @@ package com.study.zerodowntimedeploy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= DataSourceAutoConfiguration.class)
 public class ZeroDowntimeDeployApplication {
 
 	public static void main(String[] args) {
